8b5d0184ba54216d23d19d54b0653e19ba16c507
EduNagado <eduardo.nagado@gmail.com>
2025-08-22 22:31:16 -0300

    Commit da aula 22/08

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,7 @@
+function showModel(modalid){
+ document.getElementById(modalId).style.display = 'block';
+ }
+
+function hideModel(modelId){
+ document.getElementById(modalId).style.display = 'none';
+}
