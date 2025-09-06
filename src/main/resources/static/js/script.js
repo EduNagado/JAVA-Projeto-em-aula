a3072331ca7c52a6514087688dd76c4340ec6ecc
EduNagado <eduardo.nagado@gmail.com>
2025-09-05 23:12:13 -0300

    Finalização do projeto mvc com thymeleaf

--- src/main/resources/static/js/script.js
+++
@@ -1,7 +0,0 @@
-function showModel(modalid){
- document.getElementById(modalId).style.display = 'block';
- }
-
-function hideModel(modelId){
- document.getElementById(modalId).style.display = 'none';
-}
